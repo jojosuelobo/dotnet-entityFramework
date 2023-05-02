7ad760a602f9d723fb555fcd6b497ae39b8c52a6
jojosuelobo <josue.lobo@etaure.com.br>
2023-05-02 12:04:19 -0300

    Finalização pré n->n

--- Alura.Loja.Testes.ConsoleApp/Compra.cs
+++ Alura.Loja.Testes.ConsoleApp/Compra.cs
@@ -1,11 +1,9 @@
 ﻿namespace Alura.Loja.Testes.ConsoleApp
 {
-    internal class Compra
+    public class Compra
     {
-        public Compra()
-        {
-        }
-
+        public int Id { get; set; }
+        public int ProdutoId { get; set; }
         public int Quantidade { get; internal set; }
         public Produto Produto { get; internal set; }
         public double Preco { get; internal set; }
